c8880158517da8f889f6373f3acf5cdd633c2b3f
Noureddine Ettalhaouy <ettalhaoui151201@gmail.com>
2022-12-20 07:04:12 -0800

    ex04

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
-* _puts - prints a string, in reverse, followed by a new line.
+* print_rev - prints a string, in reverse, followed by a new line.
 * Owned by Ettalha0x
-* @str: string to print
-* @i: index & counter
 * Description: prints a string, in reverse, followed by a new line.
 */
 void print_rev(char *s)
@@ -13,12 +11,12 @@ void print_rev(char *s)
 
 	i = 0;
 	n = 0;
-	while(s[i] != '\0' )
+	while (s[i] != '\0')
 	{
 		i++;
 	}
 	n = i - 1;
-	while(n >= 0)
+	while (n >= 0)
 	{
 		_putchar(s[n]);
 		n--;
