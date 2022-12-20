ba1d00bb1abe03ec663e796e06c24b8728e46af8
Noureddine Ettalhaouy <ettalhaoui151201@gmail.com>
2022-12-20 07:08:02 -0800

    ex03

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,7 +3,6 @@
 * _puts - prints a string, followed by a new line, to stdout
 * Owned by Ettalha0x
 * @str: string to print
-* @i: index & counter
 * Description: this will print a string followed by a new line to stdout
 */
 void _puts(char *str)
@@ -11,7 +10,7 @@ void _puts(char *str)
 	int i;
 
 	i = 0;
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
